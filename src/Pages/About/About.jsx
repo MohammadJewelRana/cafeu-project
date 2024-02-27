d329bf62ecae156ff7d9b3f566cdfcd2c8c41d5a
MohammadJewelRana <js.rana0326@gmail.com>
2024-02-28 00:55:57 +0600

    about page design complete

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,16 +3,22 @@ import WhyBest from './WhyBest';
 import Banner from './Banner';
 import Team from './Team';
 import HappyCustomer from './HappyCustomer';
+import SectionBanner from '../../Shared/SectionBanner';
+import bannerImg from '../../assets/about/banner2.jpg'
+import Banner3 from './Banner3';
 
 const About = () => {
     return (
-        <div className='mt-32'>
-  
+        <div className='mt-20'>
 
-  <HappyCustomer></HappyCustomer>
+            <SectionBanner pageName={'About us'} path={'about'} image={bannerImg}></SectionBanner>
+          
+            
             <WhyBest></WhyBest>
             <Banner></Banner>
             <Team></Team>
+            <Banner3></Banner3>
+            <HappyCustomer></HappyCustomer>
         </div>
     );
 };
--- src/Pages/About/HappyCustomer.jsx
+++ src/Pages/About/HappyCustomer.jsx
@@ -2,20 +2,105 @@ import React from 'react';
 import Title from '../../Shared/Title';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
 
 
 const HappyCustomer = () => {
     return (
-        <div className='my-12'>
+        <div className='my-24'>
 
             <Title heading={'Our Guestbook'} subHeading={'Happy Customers'}></Title>
 
 
-            <div className='flex justify-center items-center   '>
+            <div className='   '>
 
 
 
                 <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper       shadow-lg"
+
+
+
+                >
+                    <SwiperSlide>
+                       <div className='flex gap-12 p-12 shadow-md border border-l-red-600   justify-center rounded-md'>
+                       <div>
+                            <p className='text-2xl font-bold max-w-64'>James Smith list Specification 2023</p>
+
+                            <p className='max-w-64 text-justify py-4 text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum eligendi molestias quasi beatae, dolor, nostrum soluta eum cumque modi tenetur ex ut vel ratione quo aliquam perspiciatis delectus eos debitis nihil culpa odit! Quaerat magni delectus labore nobis sit aliquid.</p>
+                            <p className='text-xl font-bold'>Reson Roy <span className='text-[12px] font-normal'>- Regular customer</span></p>
+                        </div>
+                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" className='w-64 rounded-lg' />
+                       </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                       <div className='flex gap-12 p-12 shadow-md border border-l-red-600   justify-center rounded-md'>
+                       <div>
+                            <p className='text-2xl font-bold max-w-64'>James Smith list Specification 2023</p>
+
+                            <p className='max-w-64 text-justify py-4 text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum eligendi molestias quasi beatae, dolor, nostrum soluta eum cumque modi tenetur ex ut vel ratione quo aliquam perspiciatis delectus eos debitis nihil culpa odit! Quaerat magni delectus labore nobis sit aliquid.</p>
+                            <p className='text-xl font-bold'>Reson Roy <span className='text-[12px] font-normal'>- Regular customer</span></p>
+                        </div>
+                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" className='w-64 rounded-lg' />
+                       </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                       <div className='flex gap-12 p-12 shadow-md border border-l-red-600   justify-center rounded-md'>
+                       <div>
+                            <p className='text-2xl font-bold max-w-64'>James Smith list Specification 2023</p>
+
+                            <p className='max-w-64 text-justify py-4 text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum eligendi molestias quasi beatae, dolor, nostrum soluta eum cumque modi tenetur ex ut vel ratione quo aliquam perspiciatis delectus eos debitis nihil culpa odit! Quaerat magni delectus labore nobis sit aliquid.</p>
+                            <p className='text-xl font-bold'>Reson Roy <span className='text-[12px] font-normal'>- Regular customer</span></p>
+                        </div>
+                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" className='w-64 rounded-lg' />
+                       </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                       <div className='flex gap-12 p-12 shadow-md border border-l-red-600   justify-center rounded-md'>
+                       <div>
+                            <p className='text-2xl font-bold max-w-64'>James Smith list Specification 2023</p>
+
+                            <p className='max-w-64 text-justify py-4 text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum eligendi molestias quasi beatae, dolor, nostrum soluta eum cumque modi tenetur ex ut vel ratione quo aliquam perspiciatis delectus eos debitis nihil culpa odit! Quaerat magni delectus labore nobis sit aliquid.</p>
+                            <p className='text-xl font-bold'>Reson Roy <span className='text-[12px] font-normal'>- Regular customer</span></p>
+                        </div>
+                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" className='w-64 rounded-lg' />
+                       </div>
+                    </SwiperSlide>
+
+   
+
+                </Swiper>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {/* <Swiper
                     spaceBetween={50}
                     slidesPerView={3}
                     onSlideChange={() => console.log('slide change')}
@@ -27,7 +112,7 @@ const HappyCustomer = () => {
                     <SwiperSlide>Slide 3</SwiperSlide>
                     <SwiperSlide>Slide 4</SwiperSlide>
                     ...
-                </Swiper>
+                </Swiper> */}
 
             </div>
 
